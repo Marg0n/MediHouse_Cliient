b73d3b0719a0542fdf81f9e23d3df84ce8b409a3
MarGoN <monondcosta@gmail.com>
2024-06-05 00:36:26 +0600

    added secret key

--- index.js
+++ index.js
@@ -13,13 +13,13 @@ const port = process.env.PORT || 5000;
 
 //middleware
 app.use(cors({
-    origin: [
-        'http://localhost:5173',
-        // netlify,
-        // server-side
-    ],
-    credentials: true,
-    optionsSuccessStatus: 200,
+  origin: [
+    'http://localhost:5173',
+    // netlify,
+    // server-side
+  ],
+  credentials: true,
+  optionsSuccessStatus: 200,
 }));
 app.use(express.json());
 // app.use(cookieParser());
@@ -27,51 +27,53 @@ app.use(express.json());
 // jwt validation middleware
 const verifyToken = (req, res, next) => {
 
-    const token = req?.cookies?.token;
-    // console.log(token)
-  
-    if (!token) {
-      return res.status(401).send({ message: 'Unauthorized access...' });
-    }
-  
-    if (token) {
-      jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
-        if (err) {
-          console.log(err)
-          return res.status(401).send({ message: 'Unauthorized access' });
-        }
-        // console.log(decoded)
-        req.user = decoded
-        next()
-      })
-    }
+  const token = req?.cookies?.token;
+  // console.log(token)
+
+  if (!token) {
+    return res.status(401).send({ message: 'Unauthorized access...' });
   }
+
+  if (token) {
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+      if (err) {
+        console.log(err)
+        return res.status(401).send({ message: 'Unauthorized access' });
+      }
+      // console.log(decoded)
+      req.user = decoded
+      next()
+    })
+  }
+}
 
 //creating Token
 app.post("/jwt", async (req, res) => {
-    const user = req.body;
-    console.log("user for token", user);
-    const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+  const user = req.body;
+  console.log("user for token", user);
+  const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
 
-    // res.cookie("token", token, cookieOptions).send({ success: true });
+  res
+    // .cookie("token", token, cookieOptions)
+    .send({ success: true });
 });
 
 //clearing Token
-app.get("/logout", async (req, res) => {
-    const user = req.body;
-    console.log("logging out", user);
-    res
-        // .clearCookie("token", { ...cookieOptions, maxAge: 0 })
-        .send({ success: true });
+app.post("/logout", async (req, res) => {
+  const user = req.body;
+  console.log("logging out", user);
+  res
+    // .clearCookie("token", { ...cookieOptions, maxAge: 0 })
+    .send({ success: true });
 });
 
 //routes
 app.get('/', (req, res) => {
-    res.send('Server is running');
+  res.send('Server is running');
 });
 
 app.listen(port, () => {
-    console.log(`Server listening on port: ${port}`);
+  console.log(`Server listening on port: ${port}`);
 });
 
 
@@ -94,7 +96,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     // DB Collections Connection
     const blogsCollection = client.db("mediHouseDB").collection("users");
